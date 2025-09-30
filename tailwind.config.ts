537cb00b4fc2785fe6c3c266ebe9e175d2ebc205
Christian McFadden <ch.mcfadden@ampeersenergy.de>
2025-09-30 17:16:31 +0200

    chore: migrated tailwind container util

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,6 @@ export default {
         light: '0 12px 35px rgba(0, 0 ,0 , 0.1)',
         dark: '0 6px 25px rgba(255, 255 ,255 , 0.05)',
       },
-      container: ({ theme }) => ({
-        center: true,
-        padding: {
-          DEFAULT: theme('spacing.4'),
-          sm: theme('spacing.5'),
-          lg: theme('spacing.6'),
-          xl: theme('spacing.8'),
-        },
-        screens: {
-          sm: '100%',
-          md: '100%',
-          lg: '1024px',
-          xl: '1024px',
-        },
-      }),
       fontFamily: {
         sans: [
           '"Montserrat"',
