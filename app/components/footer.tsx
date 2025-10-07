97869affa6032d514f690a240693953fcb2268e5
Christian McFadden <ch.mcfadden@ampeersenergy.de>
2025-10-07 16:51:48 +0200

    style: fix footer margin

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,12 +2,10 @@ import { Link } from 'react-router';
 
 export function Footer() {
   return (
-    <div>
-      <footer className="bg-gray-100 dark:bg-gray-800 mt-10">
-        <nav className="container py-6 flex flex-wrap flex-col sm:flex-row justify-between items-center">
-          <Link to="/legalnotice">Legal Notice</Link>
-        </nav>
-      </footer>
-    </div>
+    <footer className="bg-gray-100 dark:bg-gray-800">
+      <nav className="container py-6 flex flex-wrap flex-col sm:flex-row justify-between items-center">
+        <Link to="/legalnotice">Legal Notice</Link>
+      </nav>
+    </footer>
   );
 }
